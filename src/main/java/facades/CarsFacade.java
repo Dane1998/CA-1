092b1683f2ddac443b2fdd3d2b71156918b656fc
Lasse Birk <lassebirk@Lasses-MacBook-Pro.local>
2020-09-19 15:44:49 +0200

    Car and Index page done

--- src/main/java/facades/CarsFacade.java
+++ src/main/java/facades/CarsFacade.java
@@ -20,7 +20,12 @@ public class CarsFacade {
     private final Cars car1 = new Cars("Hans Jensen", 1992, "Volvo", "V70", 5000);
     private final Cars car2 = new Cars("Anders Mikkelsen", 1993, "Ford", "E350", 4500);
     private final Cars car3 = new Cars("Katrine Blixen", 1999, "Chevy", "Venture", 4500);
-
+    private final Cars car4 = new Cars("Mikkel Hansen", 1997, "Chevy", "Subarban", 10000);
+    private final Cars car5 = new Cars("Niller Pedersen", 1989, "Ford", "Mondeo", 11000);
+    private final Cars car6 = new Cars("Katrine Blixen", 1999, "lamborghini", "Aventador", 140000);
+    private final Cars car7 = new Cars("Katrine Blixen", 1999, "Mercedes", "V8", 4500);
+    private final Cars car8 = new Cars("Katrine Blixen", 1999, "Audi", "A7", 4500);
+    
     //Private Constructor to ensure Singleton
     private CarsFacade() {
     }
@@ -58,6 +63,12 @@ public class CarsFacade {
             em.persist(car1);
             em.persist(car2);
             em.persist(car3);
+            em.persist(car3);
+            em.persist(car4);
+            em.persist(car5);
+            em.persist(car6);
+            em.persist(car7);
+            em.persist(car8);
             em.getTransaction().commit();
         } finally {
             em.close();
